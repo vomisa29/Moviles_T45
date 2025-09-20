5e6ccbd16e03e862abf30a06d9f56be18b2b2717
vomisa29 <p.mendezm@uniandes.edu.co>
2025-09-20 01:13:44 -0500

    Login and Register Prototype views

---
+++ flutterapp/lib/register.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //Most of the structure is here
+      body:Center(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset("lib/Images/Logo_app_SportLink.png"),
+                SizedBox(height: 26),
+                TextField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                  labelText: 'Enter Email',
+                  border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 26),
+                TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                  labelText: 'Enter Password',
+                  border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 26),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 49, 177, 121),
+                    textStyle: TextStyle(
+                      
+                    ),
+                  ),
+                  child: const Text(
+                    "Continue",
+                    style: TextStyle(
+                      color: Color.fromRGBO(255, 255, 255, 1)
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Center(
+                child: Text(
+                  "Already have an account? Sign Up",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14,
+                    color: Color(0xFF87879D),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          )
+      ),
+      backgroundColor: const Color.fromARGB(255, 255, 252, 249),
+    );
+  }
+
+}
