eb94b5774e8fbc4927b945e1c4ee515c4f74bc34
Juancabel <jc.beltrang1@uniandes.edu.co>
2025-10-02 19:49:08 -0500

    Dependencies and plugins

--- flutterapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutterapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_analytics
 import firebase_core
+import firebase_crashlytics
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
