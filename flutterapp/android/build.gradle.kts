d0f9906ceeda33f3d0ecbc5f216471c145134c02
Juancabel <jc.beltrang1@uniandes.edu.co>
2025-11-01 03:23:27 -0500

    Cosas de gradle

--- flutterapp/android/build.gradle.kts
+++ flutterapp/android/build.gradle.kts
@@ -24,7 +24,7 @@ tasks.register<Delete>("clean") {
 }
 plugins {
     //id ("com.android.application") version "8.9.1" apply false
-    id ("com.google.gms.google-services") version "4.3.15" apply false
+    id ("com.google.gms.google-services") version "4.4.2" apply false
     id ("com.google.firebase.crashlytics") version "3.0.6" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
